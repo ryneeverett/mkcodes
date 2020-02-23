780cd403fa4c092dcb75cf058c3c0aeef2b5d986
Tim Hatch <tim@timhatch.com>
2020-02-23 08:18:05 -0800

    Adjust find_packages to make work in a wheel.
    Prior to this change, `setup.py bdist_wheel` does not include
    mkcodes.py, and this mechanism is used by `pip install .`
    
    Also add gitignore to make development a little cleaner.

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setuptools.setup(
     name='mkcodes',
     install_requires=['click'],
     extras_require={'markdown': ['markdown']},
-    packages=setuptools.find_packages(),
+    py_modules=['mkcodes'],
     entry_points={'console_scripts': ['mkcodes=mkcodes:main']}
 )
