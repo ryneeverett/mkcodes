863242efa11049f6e81d951c9f08c29b65c0a129
Matt Katz <git@morelightmorelight.com>
2020-08-04 16:04:36 -0400

    collect and write per-language code blocks
    as we pass through a document, get per-language codeblocks, then write
    those out to the correct extensionfile. By default, everything is still
    python - though it can be overridden

--- mkcodes.py
+++ mkcodes.py
@@ -54,7 +54,7 @@ for ext, lang_strings in ext_map.items():
 
 
 def github_codeblocks(filepath, safe, default_lang='py'):
-    codeblocks = []
+    codeblocks = {}
     codeblock_re = r'^```.*'
     codeblock_open_re = r'^```(`*)(py|python){0}$'.format('' if safe else '?')
 
@@ -72,7 +72,11 @@ def github_codeblocks(filepath, safe, default_lang='py'):
                     # we are closing a codeblock
                     if language:
                         # finished a codeblock, append everything
-                        codeblocks.append(''.join(block))
+                        # codeblocks.append(''.join(block))
+                        # import pudb; pu.db
+                        blocks = codeblocks.get(language, [])
+                        blocks.append(''.join(block))
+                        codeblocks[language] = blocks
 
                     block = []
                     if safe:
@@ -99,14 +103,14 @@ def github_codeblocks(filepath, safe, default_lang='py'):
 
 def github_markdown_codeblocks(filepath, safe, default_lang='py'):
     import markdown
-    codeblocks =[]
+    codeblocks = {}
     if safe:
         warnings.warn("'safe' option not available in 'github-markdown' mode.")
 
     class DoctestCollector(Treeprocessor):
         def run(self, root):
             nonlocal codeblocks
-            codeblocks = (block.text for block in root.iterfind('./pre/code'))
+            codeblocks[default_lang] = (block.text for block in root.iterfind('./pre/code'))
 
     class DoctestExtension(Extension):
         def extendMarkdown(self, md, md_globals):
@@ -120,10 +124,10 @@ def github_markdown_codeblocks(filepath, safe, default_lang='py'):
 
 
 
-def markdown_codeblocks(filepath, safe):
+def markdown_codeblocks(filepath, safe, default_lang='py'):
     import markdown
 
-    codeblocks = []
+    codeblocks = {}
 
     if safe:
         warnings.warn("'safe' option not available in 'markdown' mode.")
@@ -131,7 +135,7 @@ def markdown_codeblocks(filepath, safe):
     class DoctestCollector(Treeprocessor):
         def run(self, root):
             nonlocal codeblocks
-            codeblocks = (block.text for block in root.iterfind('./pre/code'))
+            codeblocks[default_lang] = (block.text for block in root.iterfind('./pre/code'))
 
     class DoctestExtension(Extension):
         def extendMarkdown(self, md, md_globals):
@@ -156,43 +160,50 @@ def get_files(inputs):
         elif path.suffix in markdown_extensions:
             yield path, path.parent
 
-def add_inits_to_dir(path):
+def add_inits_along_path(from_path, to_path):
     """Recursively add __init__.py files to a directory
     This compensates for https://bugs.python.org/issue23882 and https://bugs.python.org/issue35617
     """
-    for child in path.rglob('*'):
-        if child.is_dir():
-            (child / '__init__.py').touch()
+    to_path = to_path.expanduser().resolve()
+    from_path = from_path.expanduser().resolve()
+    if to_path.relative_to_path(from_path):
+        (to_path / '__init__.py').to_pathuch()
+        if from_path.resolve() != to_path.parent.resolve():
+            add_inits_along_path(from_path, to_path.parent()
 
 
 @click.command()
 @click.argument(
     'inputs', nargs=-1, required=True, type=click.Path(exists=True))
-@click.option('--output', default='{name}.py')
+@click.option('--output', default='{name}.{ext}')
 @click.option('--github/--markdown', default=bool(not markdown_enabled),
               help='Github-flavored fence blocks or pure markdown.')
 @click.option('--safe/--unsafe', default=True,
               help='Allow code blocks without language hints.')
 @click.option('--package-python', default=True,
               help='Add __init__.py files to python output to aid in test discovery')
-def main(inputs, output, github, safe, package_python):
+@click.option('--default_lang', default='py',
+              help='Assumed language for code blocks without language hits.')
+def main(inputs, output, github, safe, package_python, default_lang):
     collect_codeblocks = github_codeblocks if github else markdown_codeblocks
     outputbasedir = Path(output).parent
     outputbasename = Path(output).name
 
     for filepath, input_path in get_files(inputs):
-        codeblocks = collect_codeblocks(filepath, safe)
+        codeblocks = collect_codeblocks(filepath, safe, default_lang)
 
         if codeblocks:
             fp = Path(filepath)
             filedir = fp.parent.relative_to(input_path)
             filename = fp.stem
-            outputfilename = outputbasedir / filedir / outputbasename.format(name=filename)
 
-            outputfilename.parent.mkdir(parents=True, exist_ok=True)
-            outputfilename.write_text('\n\n'.join(codeblocks))
-            if package_python:
-                add_inits_to_dir(outputbasedir)
+            # stitch together the OUTPUT base directory with the input directories
+            # add the file format at the end.
+            for lang, blocks in codeblocks.items():
+                outputfilename = outputbasedir / filedir / outputbasename.format(name=filename, ext=lang)
 
-            if package_python:
-                add_inits_to_dir(outputbasedir)
+                # make sure path exists, don't care if it already does
+                outputfilename.parent.mkdir(parents=True, exist_ok=True)
+                outputfilename.write_text('\n\n'.join(blocks))
+                if package_python and lang=='py':
+                    add_inits_along_path(outputbasedir, outputfilename.parent)
